c20d4b13412d8bb80dc8d264058585a18ce8d587
dxviidmg <dmg_92@hotmail.com>
2018-01-24 18:06:53 -0600

    starting change password

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import *
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
@@ -30,3 +32,20 @@ class ViewProfile(View):
 #		if EdicionPerfilForm.is_valid:
 #			EdicionPerfilForm.save()
 #		return redirect('accounts:ViewProfile')
+
+def change_password(request):
+	if request.method == 'POST':
+		form = PasswordChangeForm(request.user, request.POST)
+		if form.is_valid():
+			user = form.save()
+			update_session_auth_hash(request, user)  # Important!
+			messages.success(request, 'Your password was successfully updated!')
+			return redirect('change_password')
+		else:
+		    messages.error(request, 'Please correct the error below.')
+	else:
+		form = PasswordChangeForm(request.user)
+	return render(request, 'accounts/change_password.html', {
+		'form': form
+	})
+
