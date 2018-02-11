a22c6dbbdc8865e8f0311c4638f45571ca070e9f
dxviidmg <dmg_92@hotmail.com>
2018-02-11 14:15:39 -0600

    admins.py

--- movies/admin.py
+++ movies/admin.py
@@ -3,6 +3,9 @@ from .models import *
 
 class MovieAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('name', 'year')}
+	list_display = ['name', 'category']
+	list_filter = ['category', 'year']
+	search_fields = ['name']
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Category)
--- tv/admin.py
+++ tv/admin.py
@@ -3,6 +3,9 @@ from .models import *
 
 class ChannelAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('name',)}
+	list_display = ['name', 'category']
+	list_filter = ['category']
+	search_fields = ['name']
 
 admin.site.register(Channel, ChannelAdmin)
 admin.site.register(Category)
