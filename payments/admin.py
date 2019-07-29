ea43f71f1010d3f807cfec69892623e12e77d59b
dxviidmg <dmg_92@hotmail.com>
2019-07-29 17:33:20 -0500

    filter date admin payments

--- payments/admin.py
+++ payments/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+	date_hierarchy = 'created'	
+#	prepopulated_fields = {'slug': ('season__number')}
+	list_display = ['user', 'created', 'quantity', 'author']
+	list_filter = ['created']
+#	search_fields = ['name']
+#	list_per_page=40
+
+admin.site.register(Payment, PaymentAdmin)
